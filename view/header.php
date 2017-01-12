2d6fb5e80fd22e948a9a67f7b67f60398fa5819b
Kerat <tarek.marce@gmail.com>
2017-01-12 02:53:25 +0100

    CSS

--- view/header.php
+++ view/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+    <link rel="icon" type="image/png" href="images/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?=$title?></title>
     <link rel="stylesheet" href="css/foundation.css" />
