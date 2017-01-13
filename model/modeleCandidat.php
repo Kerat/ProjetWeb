27f6c74da62607e26563dbe60859a38be2e578a9
FrancoisCh <francois.chaumeny@outlook.fr>
2017-01-13 15:54:18 +0100

    On est la

--- model/modeleCandidat.php
+++ model/modeleCandidat.php
@@ -57,6 +57,22 @@ require_once "User.php";
 		{
 			$req = mysqli_query($co, "UPDATE candidat SET prenom='$this->prenom', nom='$this->nom', email='$this->email', numeroTelephone='$this->tel', login='$this->login', motDePasse='$this->mdp' WHERE id_candidat=$this->idCand");
 		}
+		public static function afficherListeNom($co)
+		{
+			echo"rentré";
+			$req = mysqli_query($co, "SELECT DISTINCT(nom) FROM Candidat");
+			echo mysqli_affected_rows($co);
+			while ($data = mysqli_fetch_array($req)) {
+				echo '<option value='.$data["nom"].'>' . $data["nom"] . '</option><br/>';
+			}
+		}
+		public static function afficherListePrenom($co, $nom)
+		{
+			$req = mysqli_query($co, "SELECT prenom FROM Candidat WHERE nom=$nom");
+			while ($data = mysqli_fetch_array($req)) {
+				echo '<option value='.$data["id_candidat"].'>' . $data["prenom"] . '</option><br/>';
+			}
+		}
 		static function afficherLogin($co) {
       		$req = mysqli_query($co, 'SELECT id_candidat, login FROM candidat');
   			if (mysqli_num_rows($req) > 0) {
